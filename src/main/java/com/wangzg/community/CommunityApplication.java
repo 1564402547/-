6af97791e202d5875bc52c9386c5f6837fc73151
phb2000 <2977369447@qq.com>
2022-04-18 23:00:14 +0800

    start begin improve 2

--- src/main/java/com/wangzg/community/CommunityApplication.java
+++ src/main/java/com/wangzg/community/CommunityApplication.java
@@ -7,7 +7,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-@EnableDiscoveryClient
 public class CommunityApplication {
 
     public static void main(String[] args) {
