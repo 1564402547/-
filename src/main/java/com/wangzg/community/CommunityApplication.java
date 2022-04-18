bbd64013996bca9f280acf11a1429297d17120ff
phb2000 <2977369447@qq.com>
2022-04-18 23:35:00 +0800

    project is  running ok

--- src/main/java/com/wangzg/community/CommunityApplication.java
+++ src/main/java/com/wangzg/community/CommunityApplication.java
@@ -7,6 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableDiscoveryClient
 public class CommunityApplication {
 
     public static void main(String[] args) {
